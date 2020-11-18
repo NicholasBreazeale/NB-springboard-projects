c843c2e2a031dd3124065f1697bcda1513adaf61
Nicholas Breazeale <breazn95@gmail.com>
2020-11-18 10:10:02 -0800

    Connect4: made global variables const and completed the makeBoard function

--- connect-four/connect4.js
+++ connect-four/connect4.js
@@ -5,18 +5,24 @@
  * board fills (tie)
  */
 
-var WIDTH = 7;
-var HEIGHT = 6;
+const WIDTH = 7;
+const HEIGHT = 6;
 
-var currPlayer = 1; // active player: 1 or 2
-var board = []; // array of rows, each row is array of cells  (board[y][x])
+const currPlayer = 1; // active player: 1 or 2
+const board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
  */
 
 function makeBoard() {
-  // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+	const subarr = [];
+	for (let i = 0; i < WIDTH; i++) {
+		subarr.push(null);
+	}
+	for (let i = 0; i < HEIGHT; i++) {
+		board.push([...subarr]);
+	}
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
