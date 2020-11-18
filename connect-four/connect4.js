a98701226b8b04d5ffb89cc978dd94753269b87c
Nicholas Breazeale <breazn95@gmail.com>
2020-11-18 11:43:03 -0800

    Connect4: completed endGame function

--- connect-four/connect4.js
+++ connect-four/connect4.js
@@ -83,7 +83,7 @@ function placeInTable(y, x) {
 /** endGame: announce game end */
 
 function endGame(msg) {
-  // TODO: pop up alert message
+  alert(`Player ${currPlayer} wins!`);
 }
 
 /** handleClick: handle click of column top to play piece */
