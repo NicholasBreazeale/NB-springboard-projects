46a92b6a6feb578672c216450105407a4dfbb05c
Nicholas Breazeale <breazn95@gmail.com>
2020-12-03 10:47:59 -0800

    Giphy: added search functionality and stylizing

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -1,1 +1,12 @@
-console.log("Let's get this party started!");
+async function searchGiphy(event) {
+	event.preventDefault();
+	
+	let searchQ = document.getElementById("searchQ");
+	
+	let giphy = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchQ.value}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
+	console.log(giphy);
+
+	searchQ.value = "";
+}
+
+document.getElementById("searchForm").addEventListener("submit", searchGiphy);
