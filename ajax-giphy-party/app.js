0570579edaddaf797f585ac0ed59731176218c6a
Nicholas Breazeale <breazn95@gmail.com>
2020-12-03 11:18:09 -0800

    Giphy: fetched images are appended to the document

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -1,11 +1,17 @@
 async function searchGiphy(event) {
 	event.preventDefault();
-	
-	let searchQ = document.getElementById("searchQ");
-	
-	let giphy = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchQ.value}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
-	console.log(giphy);
 
+	let searchQ = document.getElementById("searchQ");
+
+	// Fetch a random image from Giphy
+	let giphy = await axios.get(`http://api.giphy.com/v1/gifs/random?tag=${searchQ.value}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
+
+	// Append image to document
+	let img = document.createElement("img");
+	img.setAttribute("src", `https://i.giphy.com/media/${giphy.data.data.id}/giphy.webp`);
+	document.getElementById("giphs").appendChild(img);
+
+	// Clear form
 	searchQ.value = "";
 }
 
