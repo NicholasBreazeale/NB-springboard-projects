6e6b7f2d34693cca4be66ea23aee3b39844e7e6f
Nicholas Breazeale <breazn95@gmail.com>
2020-12-16 12:01:29 -0800

    PySyntax: only words string with 'e' will be printed

--- python-syntax/words.py
+++ python-syntax/words.py
@@ -2,4 +2,5 @@ def print_upper_words(wordList):
 	"""Print out a list of words, each on separate lines, and all uppercase"""
 
 	for word in wordList:
-		print(word.upper())
+		if word[0] == "e" or word[0] == "E":
+			print(word.upper())
