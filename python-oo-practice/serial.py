bc4b4f74f171b891d5fa9a2471ee3d38cc7645a7
Nicholas Breazeale <breazn95@gmail.com>
2020-12-29 12:55:01 -0800

    PyOO: completed function assignments

--- python-oo-practice/serial.py
+++ python-oo-practice/serial.py
@@ -18,5 +18,23 @@ class SerialGenerator:
 
     >>> serial.generate()
     100
-    """
 
+    >>> serial
+    <SerialGenerator start=100 next=101>
+    """
+    def __init__(self, start=0):
+        "Set the starting value for the generator"
+        self.start = start
+        self.next = start
+
+    def __repr__(self):
+        return f"<SerialGenerator start={self.start} next={self.next}>"
+
+    def generate(self):
+        "Get the next sequential number"
+        self.next += 1
+        return self.next - 1
+
+    def reset(self):
+        "Reset the generated numbers back to the start"
+        self.next = self.start
--- python-oo-practice/wordfinder.py
+++ python-oo-practice/wordfinder.py
@@ -1,5 +1,70 @@
+import random
+
 """Word Finder: finds random words from a dictionary."""
 
 
 class WordFinder:
-    ...
+    """ List of words with random access to them
+
+    >>> wf = WordFinder("words.txt")
+    235886 words read
+
+    >>> simple = WordFinder("simple_words.txt")
+    3 words read
+
+    >>> simple.random() in ["cat", "dog", "porcupine"]
+    True
+
+    >>> simple.random() in ["cat", "dog", "porcupine"]
+    True
+
+    >>> simple.random() in ["cat", "dog", "porcupine"]
+    True
+    """
+    def __init__(self, path):
+        "Reads a list of new-line separated words from a given file"
+        self.words = []
+
+        f = open(path)
+        for word in f.read().splitlines():
+            self.words.append(word)
+        f.close()
+
+        print(f"{len(self.words)} words read")
+
+    def random(self):
+        "Return a random word from the list"
+        return random.choice(self.words)
+
+class SpecialWordFinder(WordFinder):
+    """ List of non-empty or commented words with random access to them
+
+    >>> wf = WordFinder("special_words.txt")
+    9 words read
+
+    >>> swf = SpecialWordFinder("special_words.txt")
+    4 words read
+
+    >>> swf.random() in ["kale", "parsnips", "apple", "mango"]
+    True
+
+    >>> swf.random() in ["kale", "parsnips", "apple", "mango"]
+    True
+
+    >>> swf.random() in ["kale", "parsnips", "apple", "mango"]
+    True
+
+    >>> swf.random() in ["kale", "parsnips", "apple", "mango"]
+    True
+    """
+    def __init__(self, path):
+        "Reads a list of new-line separated words from a given file, excluding empty lines and comments"
+        self.words = []
+
+        f = open(path)
+        for word in f.read().splitlines():
+            if len(word) > 0 and word[0] != "#":
+                self.words.append(word)
+        f.close()
+
+        print(f"{len(self.words)} words read")
