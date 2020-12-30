3c1320a9c9c520d64511fb01768e80e45ff28981
Nicholas Breazeale <breazn95@gmail.com>
2020-12-30 14:58:22 -0800

    FlaskGreet: completed assignments and added gitignore

--- flask-greet-calc/calc/app.py
+++ flask-greet-calc/calc/app.py
@@ -1,1 +1,11 @@
 # Put your app in here.
+from flask import Flask, request
+from operations import *
+
+app = Flask(__name__)
+
+@app.route("/math/<op>")
+def operate(op):
+    if op in ["add", "sub", "mult", "div"]:
+        return str(globals()[op](int(request.args.get("a")), int(request.args.get("b"))))
+    return "Unknown operation"
