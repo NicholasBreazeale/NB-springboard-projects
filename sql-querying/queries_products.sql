9546a7ecafeb419e64e19d43825463164ae80915
Nicholas Breazeale <breazn95@gmail.com>
2021-01-20 14:09:22 -0800

    SQL: completed playstore queries and fixed a few from products

--- sql-querying/queries_products.sql
+++ sql-querying/queries_products.sql
@@ -1,5 +1,3 @@
-\c products_db
-
 INSERT INTO products (name, price, can_be_returned) VALUES
   ('chair', 44.00, false),
   ('stool', 25.99, true),
@@ -12,7 +10,7 @@ SELECT name, price FROM products;
 INSERT INTO products (name, price, can_be_returned) VALUES
   ('sofa', 179.99, true);
 
-SELECT * FROM products WHERE can_be_returned = true;
+SELECT * FROM products WHERE can_be_returned;
 SELECT * FROM products WHERE price < 44.00;
 SELECT * FROM products WHERE price BETWEEN 22.50 AND 99.99;
 
