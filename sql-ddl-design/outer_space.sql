fb3ef027a4583baa4a0de5bde11a42a3bae4c6de
Nicholas Breazeale <breazn95@gmail.com>
2021-01-25 13:55:45 -0800

    SQLDDL: completed schema critique

--- sql-ddl-design/outer_space.sql
+++ sql-ddl-design/outer_space.sql
@@ -7,22 +7,56 @@ CREATE DATABASE outer_space;
 
 \c outer_space
 
-CREATE TABLE planets
+CREATE TABLE celestial_body_types
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE celestial_bodies
 (
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
-  orbital_period_in_years FLOAT NOT NULL,
-  orbits_around TEXT NOT NULL,
-  galaxy TEXT NOT NULL,
-  moons TEXT[]
+  classification INTEGER REFERENCES celestial_body_types NOT NULL,
+  orbits_around INTEGER REFERENCES celestial_bodies,
+  orbital_period_in_years FLOAT
 );
 
-INSERT INTO planets
-  (name, orbital_period_in_years, orbits_around, galaxy, moons)
+INSERT INTO celestial_body_types
+  (name)
 VALUES
-  ('Earth', 1.00, 'The Sun', 'Milky Way', '{"The Moon"}'),
-  ('Mars', 1.88, 'The Sun', 'Milky Way', '{"Phobos", "Deimos"}'),
-  ('Venus', 0.62, 'The Sun', 'Milky Way', '{}'),
-  ('Neptune', 164.8, 'The Sun', 'Milky Way', '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
-  ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way', '{}'),
-  ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way', '{}');
+  ('galaxy'),
+  ('star'),
+  ('planet'),
+  ('moon');
+
+INSERT INTO celestial_bodies
+  (name, classification, orbits_around, orbital_period_in_years)
+VALUES
+  ('Milky Way', 1, NULL, NULL),
+  ('The Sun', 2, 1, NULL),
+  ('Earth', 3, 2, 1.00),
+  ('The Moon', 4, 3, NULL),
+  ('Mars', 3, 2, 1.88),
+  ('Phobos', 4, 5, NULL),
+  ('Deimos', 4, 5, NULL),
+  ('Venus', 3, 2, 0.62),
+  ('Neptune', 3, 2, 164.8),
+  ('Naiad', 4, 9, NULL),
+  ('Thalassa', 4, 9, NULL),
+  ('Despina', 4, 9, NULL),
+  ('Galatea', 4, 9, NULL),
+  ('Larissa', 4, 9, NULL),
+  ('S/2004 N 1', 4, 9, NULL),
+  ('Proteus', 4, 9, NULL),
+  ('Triton', 4, 9, NULL),
+  ('Nereid', 4, 9, NULL),
+  ('Halimede', 4, 9, NULL),
+  ('Sao', 4, 9, NULL),
+  ('Laomedeia', 4, 9, NULL),
+  ('Psamathe', 4, 9, NULL),
+  ('Neso', 4, 9, NULL),
+  ('Proxima Centauri', 2, 1, NULL),
+  ('Proxima Centauri b', 3, 24, 0.03),
+  ('Gliese 876', 2, 1, NULL),
+  ('Gliese 876 b', 3, 26, 0.23);
