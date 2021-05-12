52e8594f3f4701beca2c9f27c463e77f034089fc
TesserWract <breazn95@gmail.com>
2021-05-12 12:08:18 -0700

    express-biztime: added db code

--- express-biztime/db.js
+++ express-biztime/db.js
@@ -1,2 +1,11 @@
 /** Database setup for BizTime. */
 
+const { Client } = require("pg");
+
+let db = new Client({
+  connectionString: "postgresql://postgres@localhost/biztime"
+});
+
+db.connect();
+
+module.exports = db;
