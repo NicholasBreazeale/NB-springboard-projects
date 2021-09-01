5200a6a923e8a86acffb17c4937e0a900764a043
TesserWract <breazn95@gmail.com>
2021-09-01 13:26:37 -0700

    prob-divide-conquer: completed find-rotated-index function

--- prob-divide-conquer/find-rotated-index.js
+++ prob-divide-conquer/find-rotated-index.js
@@ -1,5 +1,26 @@
-function findRotatedIndex() {
- 
+function findRotatedIndex(arr, val) {
+  // [leftIdx, rightIdx]
+  const idx = [0, arr.length - 1];
+
+  while (idx[0] <= idx[1]) {
+    const middleIdx = Math.floor((idx[0] + idx[1]) / 2);
+    const middleVal = arr[middleIdx];
+
+    // Index of val found
+    if (middleVal === val) {
+      return middleIdx;
+    }
+
+    if (arr[idx[0]] > val || middleVal < val) {
+      idx[0] = middleIdx + 1;
+    }
+    else if (arr[idx[1]] < val || middleVal > val) {
+      idx[1] = middleIdx;
+    }
+  }
+
+  // val not found
+  return -1;
 }
 
 module.exports = findRotatedIndex
