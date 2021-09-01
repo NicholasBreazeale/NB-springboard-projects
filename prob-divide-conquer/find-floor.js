84e30d1acaf57ae581320ab505c528f31f46a988
TesserWract <breazn95@gmail.com>
2021-09-01 12:11:47 -0700

    prob-divide-conquer: completed find-floor function

--- prob-divide-conquer/find-floor.js
+++ prob-divide-conquer/find-floor.js
@@ -1,5 +1,19 @@
-function findFloor() {
-  
+function findFloor(arr, val) {
+  // [leftIdx, rightIdx]
+  const idx = [0, arr.length]
+
+  while (idx[0] <= idx[1]) {
+    const middleIdx = Math.floor((idx[0] + idx[1]) / 2);
+
+    if (arr[middleIdx] < val) {
+      idx[0] = middleIdx + 1;
+    } else {
+      idx[1] = middleIdx - 1;
+    }
+  }
+
+  // If floor was not found, return -1
+  return idx[1] === -1 ? -1 : arr[idx[1]];
 }
 
 module.exports = findFloor
