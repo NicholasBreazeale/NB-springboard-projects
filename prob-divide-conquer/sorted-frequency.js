d94f27984835d1b649abe94064f3d66c4a20541e
TesserWract <breazn95@gmail.com>
2021-09-01 11:53:15 -0700

    prob-divide-conquer: completed sorted-frequency function

--- prob-divide-conquer/sorted-frequency.js
+++ prob-divide-conquer/sorted-frequency.js
@@ -1,5 +1,34 @@
-function sortedFrequency() {
+function sortedFrequency(arr, val) {
+  // [leftIdx, rightIdx]
+  const idx = [0, arr.length];
 
+  // Find the left limit
+  while (idx[0] < idx[1]) {
+    const middleIdx = Math.floor((idx[0] + idx[1]) / 2);
+
+    if (arr[middleIdx] < val) {
+      idx[0] = middleIdx + 1;
+    } else {
+      idx[1] = middleIdx;
+    }
+  }
+  const leftLimit = idx[0];
+
+  idx[1] = arr.length;
+
+  // Find the right limit
+  while (idx[0] < idx[1]) {
+    const middleIdx = Math.floor((idx[0] + idx[1]) / 2);
+
+    if (arr[middleIdx] === val) {
+      idx[0] = middleIdx + 1;
+    } else {
+      idx[1] = middleIdx;
+    }
+  }
+
+  // If number was not found, return -1
+  return (idx[0] - leftLimit) || -1;
 }
 
 module.exports = sortedFrequency
