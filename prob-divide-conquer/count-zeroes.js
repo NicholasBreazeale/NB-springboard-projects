da3f07c9db34341d6b92a827bb9083ab541e56fa
TesserWract <breazn95@gmail.com>
2021-09-01 11:17:30 -0700

    prob-divide-conquer: completed countZeroes function

--- prob-divide-conquer/count-zeroes.js
+++ prob-divide-conquer/count-zeroes.js
@@ -1,5 +1,18 @@
-function countZeroes() {
-  
+function countZeroes(arr) {
+  // [leftIdx, rightIdx]
+  const idx = [0, arr.length];
+
+  while (idx[0] < idx[1]) {
+    const middleIdx = Math.floor((idx[0] + idx[1]) / 2);
+
+    if (arr[middleIdx]) {
+      idx[0] = middleIdx + 1;
+    } else {
+      idx[1] = middleIdx;
+    }
+  }
+
+  return arr.length - idx[0];
 }
 
 module.exports = countZeroes
